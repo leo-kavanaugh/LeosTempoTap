2050127d76d06b1919913ffe319be7fe3b533cc3
Leo Kavanaugh <leokavanaugh@Leos-MacBook-Pro-3.local>
2025-05-07 22:41:28 -0400

    initial file add to repo

---
+++ code.js
@@ -0,0 +1,12 @@
+//function that converts a time duration in ms to BPM
+function convert();
+
+convert(768)
+
+
+
+// 60 / 1000ms = BPM 
+
+
+
+//function that counts time between mouse clicks in milliseconds
